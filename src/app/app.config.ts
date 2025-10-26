45ee98839e263ed1a152d6694a88d117b3e90729
Gilberto Malaga <fullgamer494@hotmail.com>
2025-10-26 08:20:10 -0600

    Configure dependency injection

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,36 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient, withInterceptorsFromDi, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { routes } from './app.routes';
+
+// Ports
+import { AuthPort } from './core/api/domain/ports/auth.port';
+import { SpotifyRepositoryPort } from './core/api/domain/ports/spotify-repository.port';
+
+// Infrastructure
+import { SpotifyAuthService } from './core/api/infrastructure/services/spotify-auth.service';
+import { SpotifyHttpService } from './core/api/infrastructure/services/spotify-http.service';
+import { AuthInterceptor } from './core/api/infrastructure/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi()),
+    
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    },
+    
+    {
+      provide: AuthPort,
+      useClass: SpotifyAuthService
+    },
+    {
+      provide: SpotifyRepositoryPort,
+      useClass: SpotifyHttpService
+    }
   ]
 };
